0a602d86bac640d2329a0b6b192ec852cad13766
Nelson Duarte <nelson.duarte.pro@gmail.com>
2026-08-19 10:08:05 +0100

    Disable slider will now work on the dual target slider
    disable_slider was bound only to the single-target. The dual-target branch will now also be disabled.

--- src/normal/climate-card.ts
+++ src/normal/climate-card.ts
@@ -606,6 +606,7 @@ export class BetterThermostatUINormalCard
           .max=${this._max}
           .step=${this._step}
           .current=${stateObj.attributes.current_temperature}
+		  .disabled=${this._config!.disable_slider}
           @low-changed=${this._lowChanged}
           @low-changing=${this._lowChanging}
           @high-changed=${this._highChanged}
